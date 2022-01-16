53c83ca3f051c5de58cee4ae251af366d1d03199
Jose Chavez <josmchavz@gmail.com>
2022-01-16 09:42:35 -0800

    added japanese

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Upload, message, Button, Row, Col } from 'antd';
 import { UploadOutlined } from '@ant-design/icons';
 import Layout, { Header, Footer, Sider, Content} from 'antd/lib/layout/layout';
-import { Menu, Dropdown } from 'antd';
+import { Menu, Dropdown, Input, Form } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import LanguageSelect from './LanguageSelect';
 import ReactDOM from "react-dom";
@@ -13,6 +13,7 @@ import i18next from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import HttpApi from 'i18next-http-backend';
 import Column from 'antd/lib/table/Column';
+import axios from 'axios';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
@@ -44,6 +45,10 @@ const languages = [
 	{
 		lang: "Spanish",
 		target: "es"
+	},
+	{
+		lang: "Japanese",
+		target: "jp"
 	}
 ];
 
@@ -81,8 +86,88 @@ const props = {
   },
 };
 
+async function uploadFile(info) {
+	if (info.file.status !== 'uploading') {
+		console.log(info.file, info.fileList);
+
+		var formData = new FormData();
+		formData.append("file", info.file);
+
+		await axios.post('http://echo.jsontest.com/key/value/one/two', formData, {
+			headers:  {
+				"Content-Type": "application/json; charset=UTF-8",
+			},
+		}).then( response => {
+			return response.data;
+		}).then(data => {
+			console.log(data);
+			info.file.status = 'done';
+		}).catch(error => {
+			info.file.status = 'error';
+			console.log(error.response.data.error);
+		});
+	}
+
+	if (info.file.status === 'done') {
+	message.success(`${info.file.name} file uploaded successfully`);
+	} else if (info.file.status === 'error') {
+	message.error(`${info.file.name} file upload failed.`);
+	}
+}
+
+// Cite: https://www.tutorialrepublic.com/javascript-tutorial/javascript-cookies.php
+function getCookie(name) {
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
+    }
+
+    // Return null if not found
+    return null;
+}
+
+function reRender() {
+	this.forceUpdate();
+}
+
+const onFinish = async (values) => {
+	const user_lang = ( getCookie("i18next") != null ) ? getCookie("i18next") : 'en';
+	values.lang = user_lang;
+	values.amount = 5;
+
+	console.log(values);
+
+	// The URL that will be sent
+	const init_url = "";
+	const last_url = `/${values.query}/${values.amount}/${values.lang}`;
+
+	// Sends to the Flask server and retrieves data
+	await axios.get('http://echo.jsontest.com/key/value/one/two', values, {
+		headers:  {
+			"Content-Type": "application/json; charset=UTF-8",
+		},
+	}).then( response => {
+		return response.data;
+	}).then(data => {
+		console.log(data);
+	}).catch(error => {
+		console.log(error);
+	});
+};
+
 function App() {
 	const { t } = useTranslation();
+	const [form] = Form.useForm();
 
   return (
 		<>
@@ -92,17 +177,24 @@ function App() {
 						<Col className="gutter-row">
 						<Dropdown overlay={menu}>
 							<a className="ant-dropdown-link" style={style} onClick={e => e.preventDefault()}>
-								Language <DownOutlined />
+								{t("Language")} <DownOutlined />
 							</a>
 						</Dropdown>
 						</Col>
 					</Row>
 				</Header>
 				<Layout className='main-content'>
-					<Upload {...props}>
-						<h2>{t('Welcome to React')}</h2>
-						<Button icon={<UploadOutlined />}>Click to Upload</Button>
-					</Upload>
+					<h2>{t('Welcome')}</h2>
+					<Form form={form} name="control-hooks" onFinish={onFinish}>
+						<Form.Item name="query" label={t("Query")} rules={[{ required: true }]}>
+							<Input />
+						</Form.Item>
+						<Form.Item>
+							<Button type="primary" htmlType="submit">
+							{t('Submit')}
+							</Button>
+						</Form.Item>
+					</Form>
 				</Layout>
 				<Footer>
 					<Row justify='center'>
